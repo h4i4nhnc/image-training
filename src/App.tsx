21bf22541989a7c4709f4a6192270564ad6daded
岡本 和大 <okamoto@Bee2binoMacBook-Pro.local>
2021-09-27 20:31:05 +0900

    20210927 RouterConsig

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/about",
+    component: About,
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route) => (
+            <Route
+              path={route.path}
+              component={route.component}
+              exact={true}
+            />
+           ))}
+        </Switch>
+      </BrowserRouter>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
   );
 }
 
